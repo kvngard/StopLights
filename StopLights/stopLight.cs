168b1e9141ef41cf094b7dcb990b7f3e1caef3cd
kvngard <kev.ngardner@gmail.com>
2014-11-14 19:11:47 -0700

    Initial commit

---
+++ StopLights/stopLight.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopLights
+{
+    class stopLight
+    {
+        public bool isGreen { get; set; }
+        public stopLight() { }
+
+    }
+}
